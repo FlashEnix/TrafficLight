5c783ae1d66929aa24977472619ffb5cca416edf
Evix <evix@inbox.ru>
2020-08-27 11:12:01 +0300

    убрал лишние атрибуты

--- Assets/TrafficLight/Scripts/TrafficLightColor.cs
+++ Assets/TrafficLight/Scripts/TrafficLightColor.cs
@@ -6,19 +6,11 @@ using UnityEngine;
 [Serializable]
 public class TrafficLightColor
 {
-    [Header("Цвет сигнала")]
-    [SerializeField]
     public Color Color;
 
-    [Header("Время действия сигнала")]
-    [SerializeField]
     public int Timer;
 
-    [Header("Описание сигнала")]
-    [SerializeField]
     public string Description;
 
-    [Header("Трафик разрешен")]
-    [SerializeField]
     public bool AllowTraffic;
 }
